bae6709e78b812c3de4c41d1acb41bae08ecaa8d
송인석 <songinseok1@gmail.com>
2022-03-17 17:41:43 +0900

    첫 기능 테스트 주석 추가 및 unittest 사용

--- functional_text.py
+++ functional_text.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome("./chromedriver")
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'install' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome("./chromedriver")
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
+        # 해당 웹 사이트를 확인하러 간다.
+        self.browser.get('http://localhost:8000')
+
+        # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다.
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+    if __name__ == '__main__':
+        unittest.main(warnings='ignore')
+
